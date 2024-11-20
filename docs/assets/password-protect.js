289e424b80b69fd10d0e5e30cbdb3f1d99d994e7
Reza <29664187+redolat@users.noreply.github.com>
2024-11-20 12:21:56 +0400

    ci: add drawio plugin

--- docs/assets/password-protect.js
+++ docs/assets/password-protect.js
@@ -1,14 +1,14 @@
-// Check if the password is already set in localStorage
-if (!localStorage.getItem('authenticated')) {
-    const password = prompt("Enter password:");
-    if (password === "bio") {
-        // Store the state in localStorage if password is correct
-        localStorage.setItem('authenticated', 'true');
-        document.body.style.display = "block";  // Show content
-    } else {
-        document.body.innerHTML = "<h1>Access Denied</h1>";
-    }
-} else {
-    // Show content if already authenticated
-    document.body.style.display = "block";  // Show content
-}
+// // Check if the password is already set in localStorage
+// if (!localStorage.getItem('authenticated')) {
+//     const password = prompt("Enter password:");
+//     if (password === "bio") {
+//         // Store the state in localStorage if password is correct
+//         localStorage.setItem('authenticated', 'true');
+//         document.body.style.display = "block";  // Show content
+//     } else {
+//         document.body.innerHTML = "<h1>Access Denied</h1>";
+//     }
+// } else {
+//     // Show content if already authenticated
+//     document.body.style.display = "block";  // Show content
+// }
